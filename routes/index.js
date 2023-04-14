e9235bebbd310f3f9ec1a3a3f279b652e19544e7
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-04-14 12:38:19 +0300

    Revert "Create"
    This reverts commit 2a2f4935fe2814284dcace2bf2d99eec708bade6.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 import {Router} from "express";
 import authRoutes from "./auth.js";
-import userRoutes from "./user.js";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
-router.use("/user", userRoutes);
 
 export default router;

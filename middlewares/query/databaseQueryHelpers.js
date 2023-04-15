3fe9ddf72ef6d0e58c03250c0b6f60a646ef6106
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-04-15 14:06:21 +0300

    Create checkGenreExists query middleware

--- middlewares/query/databaseQueryHelpers.js
+++ middlewares/query/databaseQueryHelpers.js
@@ -1,5 +1,7 @@
 import expressAsyncHandler from "express-async-handler";
 import User from "../../models/User.js";
+import Genre from "../../models/Genre.js";
+import CustomError from "../../helpers/error/CustomError.js";
 
 
 export const checkUserExists = expressAsyncHandler(async(req, res, next) => {
@@ -15,4 +17,20 @@ export const checkUserExists = expressAsyncHandler(async(req, res, next) => {
     }
 
     next();
+});
+
+export const checkGenreExists = expressAsyncHandler(async(req, res, next) => {
+
+    const {genreId} = req.params;
+
+    const isGenreExists = await Genre.findOne({where: {
+        id: genreId
+    }});
+
+    if(!isGenreExists){
+        return next(new CustomError(404, "Genre was not found with that id"));
+    }
+
+    next();
+
 });

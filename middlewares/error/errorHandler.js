9e317607ed7b90806be31446f963d23c7c63ff3b
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-04-15 14:04:36 +0300

    Update errorHandler

--- middlewares/error/errorHandler.js
+++ middlewares/error/errorHandler.js
@@ -4,6 +4,10 @@ export const errorHandler = (err, req, res, next) => {
     
     const error = new CustomError(err.status || 500, err.message);
 
+    if(error.message.includes("jwt")){
+        error.message = "Your token has been expired. Please log in."
+    }
+
     return res
     .status(error.status)
     .json({

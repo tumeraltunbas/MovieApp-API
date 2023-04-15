fd0719b13b7060a605f2d37f5745ff1aee57b9f7
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-04-15 14:06:45 +0300

    Create getAdminAccess middleware

--- middlewares/auth/auth.js
+++ middlewares/auth/auth.js
@@ -1,4 +1,7 @@
 import jwt from "jsonwebtoken";
+import expressAsyncHandler from "express-async-handler";
+import User from "../../models/User.js";
+import CustomError from "../../helpers/error/CustomError.js";
 
 export const isAuth = (req, res, next) => {
 
@@ -7,6 +10,10 @@ export const isAuth = (req, res, next) => {
 
     jwt.verify(token, JWT_SECRET_KEY, (err, decoded) => {
         
+        if(err){
+            return next(err);
+        }
+
         req.user = {
             id: decoded.id,
             email: decoded.email
@@ -16,3 +23,16 @@ export const isAuth = (req, res, next) => {
     });
     
 }
+
+export const getAdminAccess = expressAsyncHandler(async(req, res, next) => {
+
+    const user = await User.findOne({where : {
+        id: req.user.id
+    }});
+
+    if(!user.isAdmin) {
+        return next(new CustomError(403, "Only admins can access this route"));
+    }
+
+    next();
+});

dbba2f4d36983eae212fe5d644467371490ca316
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-04-15 14:19:07 +0300

    Update editGenre endpoint

--- controllers/genre.js
+++ controllers/genre.js
@@ -35,6 +35,14 @@ export const editGenre = expressAsyncHandler(async(req, res, next) => {
     const {genreId} = req.params;
     const {genreName} = req.body;
 
+    const isGenreExist = await Genre.findOne({where: {
+        name: genreName
+    }});
+
+    if(isGenreExist){
+        return next(new CustomError(400, "This genre name is already exists"));
+    }
+
     const genre = await Genre.findOne({where: {
         id: genreId
     }});

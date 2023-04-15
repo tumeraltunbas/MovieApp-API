7f819fd9a029dd7b84a561948c369c77dff832a0
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-04-15 17:20:31 +0300

    Create checkRoleExists middleware

--- middlewares/query/databaseQueryHelpers.js
+++ middlewares/query/databaseQueryHelpers.js
@@ -2,6 +2,7 @@ import expressAsyncHandler from "express-async-handler";
 import User from "../../models/User.js";
 import Genre from "../../models/Genre.js";
 import CustomError from "../../helpers/error/CustomError.js";
+import Role from "../../models/Role.js";
 
 
 export const checkUserExists = expressAsyncHandler(async(req, res, next) => {
@@ -24,11 +25,29 @@ export const checkGenreExists = expressAsyncHandler(async(req, res, next) => {
     const {genreId} = req.params;
 
     const isGenreExists = await Genre.findOne({where: {
-        id: genreId
+        id: genreId,
+        isVisible: true
     }});
 
     if(!isGenreExists){
         return next(new CustomError(404, "Genre was not found with that id"));
+    }
+
+    next();
+
+});
+
+export const checkRoleExists = expressAsyncHandler(async(req, res, next) => {
+
+    const {roleId} = req.body;
+
+    const isRoleExists = await Role.findOne({where: {
+        id: roleId,
+        isVisible: true
+    }});
+
+    if(!isRoleExists){
+        return next(new CustomError(404, "Role was not found with that id"));
     }
 
     next();

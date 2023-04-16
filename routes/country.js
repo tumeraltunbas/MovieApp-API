0b884e9788045fb8437f91a96110ee4b79d4881c
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-04-16 12:49:13 +0300

    Create deleteCountry endpoint

--- routes/country.js
+++ routes/country.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { createCountry, editCountry } from "../controllers/country.js";
+import { createCountry, deleteCountry, editCountry } from "../controllers/country.js";
 import { getAdminAccess, isAuth } from "../middlewares/auth/auth.js";
 import { checkCountryExists } from "../middlewares/query/databaseQueryHelpers.js";
 
@@ -7,5 +7,6 @@ const router = Router();
 
 router.post("/", [isAuth, getAdminAccess], createCountry);
 router.put("/:countryId", [isAuth, getAdminAccess, checkCountryExists], editCountry);
+router.put("/:countryId/delete", [isAuth, getAdminAccess, checkCountryExists], deleteCountry);
 
 export default router;

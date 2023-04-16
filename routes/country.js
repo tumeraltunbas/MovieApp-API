436c9056576b86120ed0bf90830b1192f46aef25
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-04-16 12:37:27 +0300

    Add middlewares to createCountry

--- routes/country.js
+++ routes/country.js
@@ -1,8 +1,9 @@
 import {Router} from "express";
 import { createCountry } from "../controllers/country.js";
+import { getAdminAccess, isAuth } from "../middlewares/auth/auth.js";
 
 const router = Router();
 
-router.post("/", createCountry);
+router.post("/", [isAuth, getAdminAccess], createCountry);
 
 export default router;

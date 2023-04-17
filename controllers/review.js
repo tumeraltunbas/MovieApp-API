23b98acd0daf4dcb250dee629796e98b24dff5fd
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-04-17 14:52:32 +0300

    Create editReview endpoint

--- controllers/review.js
+++ controllers/review.js
@@ -21,3 +21,24 @@ export const createReview = expressAsyncHandler(async(req, res, next) => {
     });
 
 })
+
+export const editReview = expressAsyncHandler(async(req, res, next) => {
+
+    const updateInformations = req.body;
+    const {reviewId} = req.params;
+
+    const review = await Review.findOne({where: {
+        id: reviewId,
+        isVisible: true
+    }});
+
+    await review.update({...updateInformations});
+
+    return res
+    .status(200)
+    .json({
+        success: true,
+        message: "Review has been updated"
+    });
+
+});
--- routes/review.js
+++ routes/review.js
@@ -1,9 +1,11 @@
 import {Router} from "express";
-import { isAuth } from "../middlewares/auth/auth.js";
-import { createReview } from "../controllers/review.js";
+import { getReviewOwnerAccess, isAuth } from "../middlewares/auth/auth.js";
+import { createReview, editReview } from "../controllers/review.js";
+import { isReviewExists } from "../middlewares/query/databaseQueryHelpers.js";
 
 const router = Router({mergeParams: true});
 
 router.post("/", isAuth, createReview);
+router.put("/:reviewId", [isAuth, isReviewExists, getReviewOwnerAccess], editReview);
 
 export default router;

0ae545b1c0b031123f4defc35cf67e2292049fae
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-04-17 13:02:38 +0300

    Create getAllDirectors endpoit

--- controllers/staff.js
+++ controllers/staff.js
@@ -114,24 +114,6 @@ export const getAllStaffs = expressAsyncHandler(async(req, res, next) => {
 
 });
 
-export const getStaffById = expressAsyncHandler(async(req, res, next) => {
-
-    const {staffId} = req.params;
-
-    const staff = await Staff.findOne({where: {
-        id: staffId,
-        isVisible: true
-    }});
-
-    return res
-    .status(200)
-    .json({
-        success: true,
-        staff: staff
-    });
-
-});
-
 export const getAllActors = expressAsyncHandler(async(req, res, next) => {
 
     const actors = await Staff.findAll({
@@ -146,6 +128,45 @@ export const getAllActors = expressAsyncHandler(async(req, res, next) => {
     .json({
         success: true,
         actors: actors
+    });
+
+});
+
+export const getAllDirectors = expressAsyncHandler(async(req, res, next) => {
+
+    const directors = await Staff.findAll({
+        where: {
+            isVisible: true,
+        },
+        include: {
+            model: Role,
+            where: { name: "Director" }
+        }
+    });
+
+    return res
+    .status(200)
+    .json({
+        success: true,
+        directors: directors
+    });
+
+});
+
+export const getStaffById = expressAsyncHandler(async(req, res, next) => {
+
+    const {staffId} = req.params;
+
+    const staff = await Staff.findOne({where: {
+        id: staffId,
+        isVisible: true
+    }});
+
+    return res
+    .status(200)
+    .json({
+        success: true,
+        staff: staff
     });
 
 });
--- routes/staff.js
+++ routes/staff.js
@@ -1,7 +1,7 @@
 import {Router} from "express";
 import {isAuth, getAdminAccess} from "../middlewares/auth/auth.js";
 import upload from "../helpers/multer/multer.js";
-import { createStaff, editStaff, deleteStaff, getAllStaffs, getStaffById, getAllActors, getStaffsByCountryId } from "../controllers/staff.js";
+import { createStaff, editStaff, deleteStaff, getAllStaffs, getAllActors, getAllDirectors, getStaffById, getStaffsByCountryId } from "../controllers/staff.js";
 import { checkCountryExists, checkStaffExists } from "../middlewares/query/databaseQueryHelpers.js";
 
 const router = Router();
@@ -12,6 +12,7 @@ router.put("/:staffId/delete", [isAuth, getAdminAccess, checkStaffExists], delet
 
 router.get("/", getAllStaffs);
 router.get("/actors", getAllActors);
+router.get("/directors", getAllDirectors);
 router.get("/:staffId", checkStaffExists, getStaffById);
 router.get("/country/:countryId", checkCountryExists,getStaffsByCountryId);
 

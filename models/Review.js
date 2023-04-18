15ab186c9f2d91fddf8942e6591b74849a70f4cb
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-04-18 14:38:27 +0300

    Add hook

--- models/Review.js
+++ models/Review.js
@@ -1,5 +1,6 @@
 import { DataTypes } from "sequelize";
 import {sequelize} from "../helpers/database/database.js";
+import Movie from "./Movie.js";
 
 const Review = sequelize.define("Review", {
 
@@ -31,6 +32,40 @@ const Review = sequelize.define("Review", {
         type: DataTypes.DATE,
         defaultValue: DataTypes.NOW
     },
+});
+
+//Hooks
+Review.addHook("afterSave", async function(review){
+
+    if(review.changed("rating")){
+
+        const sumRating = await Review.sum("rating", {
+            where: { 
+                MovieId: review.MovieId, 
+                isVisible: true 
+            }
+        });
+
+        const reviewCount = await Review.count({
+            where: {
+                MovieId: review.MovieId, 
+                isVisible: true 
+            }
+        });
+    
+        const averageRating = Number(sumRating / reviewCount).toFixed(1);
+        
+        const movie = await Movie.findOne({
+            where: {
+                id: review.MovieId
+            }
+        });
+
+        movie.rating = averageRating;
+
+        await movie.save();
+        
+    }
 });
 
 await Review.sync();

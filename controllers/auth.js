0f7987f3e3953d3aa7b486f94b2c9ff71ae2a49e
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-04-18 15:24:10 +0300

    Delete comment lines

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,44 +7,7 @@ import CustomError from "../helpers/error/CustomError.js";
 import { Op } from "sequelize";
 import bcrypt from "bcryptjs";
 
-export const signUp = expressAsyncHandler(async(req, res, next) => {
-
-    // const {firstName, lastName, email, password} = req.body;
-    // const {EMAIL_VERIFICATION_TOKEN_EXPIRES, SMTP_USER, DOMAIN} = process.env;
-
-    // const existingUser = await User.findOne({where: {
-    //     email: email
-    // }});
-
-    // const emailVerificationToken = createToken();
-
-    // if(existingUser && existingUser.isRegisterCompleted != true) {
-
-    //     existingUser.emailVerificationToken = emailVerificationToken;
-    //     existingUser.emailVerificationTokenExpires = new Date(Date.now() + Number(EMAIL_VERIFICATION_TOKEN_EXPIRES));
-
-    //     await existingUser.save();
-    // }
-    // else{
-
-    //     const newUser = await User.create({
-    //         firstName: firstName, 
-    //         lastName:lastName,
-    //         email:email,
-    //         password: password
-    //     });
-        
-    //     if(!validatePassword(password)){
-    //         return next(new CustomError(400, "Your password must contains: Minimum eight characters, at least one uppercase letter, one lowercase letter and one number."));
-    //     }
-
-    //     newUser.emailVerificationToken = emailVerificationToken;
-    //     newUser.emailVerificationTokenExpires = new Date(Date.now() + Number(EMAIL_VERIFICATION_TOKEN_EXPIRES));
-    
-    //     await newUser.save();
-    // }
-
-    // sendEmailVerificationTokenToUser(email, emailVerificationToken);
+export const signUp = expressAsyncHandler(async(req, res, next) => {g
 
     const {firstName, lastName, email, password} = req.body;
 

45c985353c7bd796fb21cb16d9ca95fecf15a9cc
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-04-21 14:37:37 +0300

    Create getFavoritesByMovieId endpoint

--- controllers/favorite.js
+++ controllers/favorite.js
@@ -2,6 +2,7 @@ import expressAsyncHandler from "express-async-handler";
 import Favorite from "../models/Favorite.js";
 import Movie from "../models/Movie.js";
 import CustomError from "../helpers/error/CustomError.js";
+import User from "../models/User.js";
 
 export const createFavorite = expressAsyncHandler(async(req, res, next) => {
 
@@ -50,6 +51,30 @@ export const deleteFavorite = expressAsyncHandler(async(req, res, next) => {
     .json({
         success: true,
         message: "Favorite has been deleted"
+    });
+
+});
+
+export const getFavoritesByMovieId = expressAsyncHandler(async(req, res, next) => {
+
+    const {movieId} = req.params;
+
+    const favorites = await Favorite.findAll({
+        where: {
+            MovieId: movieId 
+        },
+        include: {
+            model: User,
+            attributes: ["id", "firstName", "lastName"]
+        }
+    });
+
+    return res
+    .status(200)
+    .json({
+        success: true,
+        favorites: favorites,
+        favoriteCount: favorites.length
     });
 
 });
--- routes/favorite.js
+++ routes/favorite.js
@@ -1,13 +1,16 @@
 import {Router} from "express";
 import {isAuth} from "../middlewares/auth/auth.js";
-import { createFavorite, deleteFavorite } from "../controllers/favorite.js";
+import { createFavorite, deleteFavorite, getFavoritesByMovieId } from "../controllers/favorite.js";
 import { checkMovieExists, checkFavoriteExists } from "../middlewares/query/databaseQueryHelpers.js";
 
 
 const router = Router({mergeParams: true});
 
-router.get("/", [isAuth, checkMovieExists], createFavorite);
+router.get("/create", [isAuth, checkMovieExists], createFavorite);
 router.delete("/", [isAuth, checkMovieExists, checkFavoriteExists], deleteFavorite);
+
+router.get("/", checkMovieExists, getFavoritesByMovieId);
+
 
 
 export default router; 

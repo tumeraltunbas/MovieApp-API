a11c37397d53ce83f86e36da0e5bfb6902aef3f9
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-04-22 15:42:53 +0300

    Create check2FA helper

--- helpers/input/inputHelpers.js
+++ helpers/input/inputHelpers.js
@@ -1,3 +1,5 @@
+import speakeasy from "speakeasy";
+
 export const validatePassword = (password) => {
     
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
@@ -8,5 +10,15 @@ export const validatePassword = (password) => {
 export const capitalize = (text) => {
     
     return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+
+}
+
+export const check2FA = (secret, token) => {
+
+    return speakeasy.totp.verify({
+        encoding: "base32",
+        secret: secret,
+        token: token,
+    });
 
 }
